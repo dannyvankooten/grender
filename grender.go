ed80bcc70516231c09991a9a3261e2e73c7bad73
Danny van Kooten <dannyvankooten@gmail.com>
2017-03-25 18:58:47 +0100

    add funcs for rendering XML and Text data

--- grender.go
+++ grender.go
@@ -3,6 +3,7 @@ package grender
 import (
 	"bufio"
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -20,6 +21,12 @@ const (
 
 	// ContentType HTTP header name for defining the content type
 	ContentType = "Content-Type"
+
+	// ContentText header value for Text data.
+	ContentText = "text/plain"
+
+	// ContentXML header value for XML data.
+	ContentXML = "text/xml"
 
 	// DefaultCharset for when no specific Charset options was given
 	DefaultCharset = "UTF-8"
@@ -121,6 +128,28 @@ func (r *Grender) JSON(w http.ResponseWriter, statusCode int, data interface{}) 
 	w.WriteHeader(statusCode)
 	err := json.NewEncoder(w).Encode(data)
 	return err
+}
+
+// XML writes the data as a XML HTTP response to the ResponseWriter
+func (r *Grender) XML(w http.ResponseWriter, statusCode int, data interface{}) error {
+	w.Header().Set("Content-Type", ContentXML+"; charset="+r.options.Charset)
+
+	// do nothing if nil data
+	if data == nil {
+		return nil
+	}
+
+	w.WriteHeader(statusCode)
+	err := xml.NewEncoder(w).Encode(data)
+	return err
+}
+
+// Text writes the data as a JSON HTTP response to the ResponseWriter
+func (r *Grender) Text(w http.ResponseWriter, statusCode int, data string) error {
+	w.Header().Set("Content-Type", ContentText+"; charset="+r.options.Charset)
+	w.WriteHeader(statusCode)
+	w.Write([]byte(data))
+	return nil
 }
 
 func (r *Grender) compileTemplatesFromDir() {
